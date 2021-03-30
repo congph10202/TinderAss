2f927fec0d9bbf2fcc46f8a8e5619f2d20a88350
= <Manhdvph10986@fpt.edu.vn>
2021-03-31 01:33:33 +0700

    them file index.php

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
- var http = require('http');
- var fs = require('fs');
+//  var http = require('http');
+//  var fs = require('fs');
+//
+// http.createServer(function (request, response){
+//     response.writeHead('200',{'Content-Type':'text/html'});
+//
+//     fs.readFile('index.html',function (error,data){
+//         if(error != null){
+//                 response.end(error);
+//         }else {
+//             response.write(data);
+//             response.end();
+//         }
+//     })
+// }).listen((process.env.PORT));
 
-http.createServer(function (request, response){
-    response.writeHead('200',{'Content-Type':'text/html'});
+var express = require('express');
+var expressHBS = require('express-handlebars');
 
-    fs.readFile('index.html',function (error,data){
-        if(error != null){
-                response.end(error);
-        }else {
-            response.write(data);
-            response.end();
-        }
-    })
-}).listen((process.env.PORT));
+var app =express();
+app.listen(4000);
+app.use(express.static('css_new'));
+
+app.engine('.handlebars', expressHBS());
+
+app.set('view engine','.handlebars');
+
+app.get('/home', function (req,res){
+   res.render('index'); //file name
+});
+app.get('/signup', function (req,res){
+   res.render('signup'); //file name
+});
+app.get('/signin', function (req,res){
+   res.render('signin'); //file name
+});
