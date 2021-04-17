bd2ebf78cd78e2daa41bacdb0ee84b5ccc552274
= <Manhdvph10986@fpt.edu.vn>
2021-04-18 02:17:31 +0700

    up lab 6+7(chua fix lan nao)(0)

--- index.js
+++ index.js
@@ -1,38 +1,203 @@
-//  var http = require('http');
-//  var fs = require('fs');
-//
-// http.createServer(function (request, response){
-//     response.writeHead('200',{'Content-Type':'text/html'});
-//
-//     fs.readFile('index.html',function (error,data){
-//         if(error != null){
-//                 response.end(error);
-//         }else {
-//             response.write(data);
-//             response.end();
-//         }
-//     })
-// }).listen((process.env.PORT));
 
 var express = require('express');
 var expressHBS = require('express-handlebars');
-
 var app =express();
-app.listen(process.env.PORT || 3000);
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json());
+// mongoose
+const uri = "mongodb+srv://admin:admin@cluster0.zhsjs.mongodb.net/tinder?retryWrites=true&w=majority";
+// getting-started.js
+const mongoose = require('mongoose');
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+   // we're connected!
+   console.log('Connected'); // kiểm tra xem đã kết nối được với thư viện mogoose chưa
+});
+
+var multer = require('multer');
+//uploads single
+var upload = multer({
+   dest:'uploads/',
+});
+var user = new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String,
+    age: String,
+    phone: String,
+    address: String,
+    hobbies : String,
+    pet :String,
+})
+ //
+app.listen(process.env.PORT  );
+// console.log(`http://localhost:${7000}`);
 app.use(express.static('css_new'));
 
 app.engine('.handlebars', expressHBS());
 
 app.set('view engine','.handlebars');
+
+// get("/") ---
 app.get('', function (req,res){
    res.render('index'); //file name
 });
-app.get('/home', function (req,res){
-   res.render('index'); //file name
-});
-app.get('/signup.handlebars', function (req,res){
+app.get('/signup', function (req,res){
    res.render('signup'); //file name
 });
-app.get('/signin.handlebars', function (req,res){
-   res.render('signin'); //file name
+app.get('/signin', function (req,res){
+    res.render('signin'); //file name
 });
+
+
+app.get('/infor', function (req,res){
+    res.render('myInfor'); //file name
+});
+app.get('/l_oStart', function (req,res){
+    res.render('layoutStart'); //file name
+});
+app.get('/home', function (req,res){
+    res.render('home'); //file name
+});
+app.get('/add_user', function (req,res){
+    res.render('addUser'); //file name
+});
+
+// post login lên mogoose
+app.post('/home', function (req,res){
+
+    var userConnect = db.model('tests', user);
+    userConnect({
+        username:req.body.username,
+        password:req.body.password,
+    }).save(function (error) {
+        if (error) {
+            console.log('Loi' + error);
+
+        } else {
+            res.render('home',{loi:'thanh cong'});
+            console.log('Thanh cong')
+        }
+    })
+    console.log(req.body.userName);
+});
+//post data user lên mogoose
+app.post('/addUsers', function (req,res){
+        //add user
+        var userConnect = db.model('user_names', user);
+        userConnect({
+            username:req.body.username,
+            name :req.body.name,
+            age :req.body.age,
+            address :req.body.address,
+            phone :req.body.phone,
+            hobbies :req.body.hobbies,
+            pet : req.body.hobbies,
+        }).save(function (error) {
+            if (error) {
+                console.log('Loi' + error);
+            } else {
+                res.render('addUser',{loi:'thanh cong'});
+                console.log('Thanh cong');
+                console.log(req.body);
+            }
+        })
+        console.log()
+
+
+});
+//function updateUsers
+function updateUsers(req,res){
+    var userModel = db.model('user_names', user);
+    userModel.findOneAndUpdate({_id:req.body},req.body,{new : true},( err, doc)=>{
+        if(!err){
+            res.redirect('l_user');
+        }else {
+            console.log(err);
+        }
+    })
+}
+
+//get all data user
+app.get('/l_user', (req, res)=>{
+    var userModel = db.model('user_names', user);
+        userModel.find({})
+            .then(userlist => {
+            res.render('listUser',{
+                ups:userlist.map(user=>user.toJSON())
+            });
+        })
+})
+
+//update
+app.get('/:id',(req,res)=>{
+    var userModel = db.model('user_names', user);
+    userModel.findById(req.params.id,(err,user)=>{
+        if(!err){
+            res.render('myInfor',{
+                viewTitle : 'Update User',
+                user:user.toJSON()
+            })
+        }
+    })
+})
+//post update
+app.post('/updateuser',(req,res)=>{
+    var userModel = db.model('user_names', user);
+    userModel.findOneAndUpdate({_id:req.body.id},req.body,{new : true},( err, doc)=>{
+        if(!err){
+            res.redirect('l_user');
+            console.log("ok");
+        }else {
+            console.log(err);
+        }
+    })
+})
+
+//delete
+// get delete
+app.get('/delete/:id', async (req,res)=>{
+
+        try {
+            var userModel = db.model('user_names', user);
+            const users = await userModel.findByIdAndDelete(req.params.id,req.body);
+            if(!users){
+                res.status(404).send('No item found');
+            }
+            else{
+                res.redirect('/list')
+            }
+        }
+        catch (err){
+            res.status(500).send(err);
+        }
+
+    // try{
+    //     var userModel = db.model('user_names', user);
+    //     const users = userModel.findByIdAndDelete(req.params.id, req.body);
+    //     if(!users)res.status(404).send('No item found');
+    //     res.status(200).send();
+    // }catch (error){
+    //     res.status(500).send(error);
+    // }
+})
+//delete
+// app.delete('/delete/:id', async (req, res) =>{
+//     try{
+//         var userModel = db.model('user_names', user);
+//         const users = await userModel.findByIdAndDelete(req.params.id, req.body);
+//         userModel.findByIdAndDelete(req.params.id, req.body);
+//         if(!users) res.status(404).send('No item found');
+//         res.status(200).send();
+//     }catch (error){
+//         res.status(500).send(error);
+//     }
+// })
+
+
